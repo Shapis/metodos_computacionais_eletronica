954fd8322841f410cd5dabf1a5e1a57ba484dcce
Henrique Silva <hpsilva+git@pm.me>
2021-10-08 03:38:05 -0300

    Trying to upload package

---
+++ datastructures/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='datastructures',
+    version='0.0.1',
+    author='hpsilva',
+    author_email='hpsilva@protonmail.com',
+    description='Package com estruturas de dados da materia metodos computacionais',
+    url='https://github.com/Shapis/metodos_computacionais_eletronica',
+    packages=['datastructures'],
+)
